5d78ccde0733e3752e1d47a376d8b2bb41c98fde
Alykhan Jetha <aj@marketcircle.com>
2017-12-12 11:44:31 -0500

    Initial Commit

---
+++ TestSwiftApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TestSwiftApp
+//
+//  Created by Alykhan Jetha on 2017-12-12.
+//  Copyright © 2017 Marketcircle. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
